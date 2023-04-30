8c8d9f1aa3537260b83d0adbd2e52d14c4419551
sweeneyb <tab126@gmail.com>
2023-04-30 12:14:27 -0400

    works with google and github providers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+          Log Out
+        </button>
+        {isAuthenticated && (
+          <div>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        )}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,3 +41,4 @@ function App() {
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-yfq46p15s2hh2xfd.us.auth0.com"
+      clientId="dlTSv0M4iJ5hzjVUcH3fWSjz6TxpUA5f"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
